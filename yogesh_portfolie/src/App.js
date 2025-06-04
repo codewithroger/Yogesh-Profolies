be0e5f999f63b5fa237c46faf3d46114971d2ab5
Roger Code <yogeshhitnalli96@gmail.com>
2025-06-04 22:53:54 +0530

    Inital Comment

---
+++ yogesh_portfolie/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Portfolie from "./Component/Portfolie"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+   <Portfolie/>
+    </div>
+  );
+}
+
+export default App;
